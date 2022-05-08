e584b850253fc3e2ddef7c6c2752435b2321afa5
dantaiti <danbansan0421@gmail.com>
2022-05-08 17:52:57 +0900

    fix :　スロットのリール回転の時間
    スロット回転の値など変更

--- Assets/Soroeru/Scripts/InGame/Application/Const.cs
+++ Assets/Soroeru/Scripts/InGame/Application/Const.cs
@@ -22,7 +22,7 @@ namespace Soroeru.InGame
     public sealed class SlotConfig
     {
         public const float REEL_ROTATE_SPEED = 1.0f;
-        public const float REEL_ROTATE_INTERVAL = 5.0f;
+        public const float REEL_ROTATE_INTERVAL = 2.0f;
     }
 
     public sealed class ItemConfig
